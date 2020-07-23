765e594cb94e05d8f66cc1dcfbf2c62f260c9f6c
byzelg <beyzanurelgun@gmail.com>
2020-07-23 18:31:14 +0300

    Add project files.

---
+++ SimpleSteganography/Form2.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace SimpleSteganography
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.buttonEncode = new System.Windows.Forms.Button();
+            this.buttonDecode = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MSELabel = new System.Windows.Forms.Label();
+            this.PSNRLabel = new System.Windows.Forms.Label();
+            this.mesajLabel = new System.Windows.Forms.Label();
+            this.buttonRGB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(119, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 185);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(235, 293);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFilePath.TabIndex = 2;
+            // 
+            // buttonEncode
+            // 
+            this.buttonEncode.Location = new System.Drawing.Point(661, 87);
+            this.buttonEncode.Name = "buttonEncode";
+            this.buttonEncode.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncode.TabIndex = 3;
+            this.buttonEncode.Text = "Encode";
+            this.buttonEncode.UseVisualStyleBackColor = true;
+            this.buttonEncode.Click += new System.EventHandler(this.buttonEncode_Click);
+            // 
+            // buttonDecode
+            // 
+            this.buttonDecode.Location = new System.Drawing.Point(661, 180);
+            this.buttonDecode.Name = "buttonDecode";
+            this.buttonDecode.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecode.TabIndex = 4;
+            this.buttonDecode.Text = "Decode";
+            this.buttonDecode.UseVisualStyleBackColor = true;
+            this.buttonDecode.Click += new System.EventHandler(this.buttonDecode_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(521, 129);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMessage.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(409, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Message:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "MSE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "PSNR";
+            // 
+            // MSELabel
+            // 
+            this.MSELabel.AutoSize = true;
+            this.MSELabel.Location = new System.Drawing.Point(235, 348);
+            this.MSELabel.Name = "MSELabel";
+            this.MSELabel.Size = new System.Drawing.Size(0, 13);
+            this.MSELabel.TabIndex = 9;
+            // 
+            // PSNRLabel
+            // 
+            this.PSNRLabel.AutoSize = true;
+            this.PSNRLabel.Location = new System.Drawing.Point(235, 388);
+            this.PSNRLabel.Name = "PSNRLabel";
+            this.PSNRLabel.Size = new System.Drawing.Size(0, 13);
+            this.PSNRLabel.TabIndex = 10;
+            // 
+            // mesajLabel
+            // 
+            this.mesajLabel.AutoSize = true;
+            this.mesajLabel.Location = new System.Drawing.Point(12, 428);
+            this.mesajLabel.Name = "mesajLabel";
+            this.mesajLabel.Size = new System.Drawing.Size(0, 13);
+            this.mesajLabel.TabIndex = 11;
+            // 
+            // buttonRGB
+            // 
+            this.buttonRGB.Location = new System.Drawing.Point(521, 293);
+            this.buttonRGB.Name = "buttonRGB";
+            this.buttonRGB.Size = new System.Drawing.Size(129, 42);
+            this.buttonRGB.TabIndex = 12;
+            this.buttonRGB.Text = "RGB Değerleri";
+            this.buttonRGB.UseVisualStyleBackColor = true;
+            this.buttonRGB.Click += new System.EventHandler(this.buttonRGB_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRGB);
+            this.Controls.Add(this.mesajLabel);
+            this.Controls.Add(this.PSNRLabel);
+            this.Controls.Add(this.MSELabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonDecode);
+            this.Controls.Add(this.buttonEncode);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button buttonEncode;
+        private System.Windows.Forms.Button buttonDecode;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label MSELabel;
+        private System.Windows.Forms.Label PSNRLabel;
+        private System.Windows.Forms.Label mesajLabel;
+        private System.Windows.Forms.Button buttonRGB;
+    }
+}
---
+++ SimpleSteganography/Form2.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SimpleSteganography
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MSELabel.Text = ""; //temizle
+            PSNRLabel.Text = "";
+
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "İmage Files (*.png, *.jpg) | *.png; *.jpg";
+            openDialog.InitialDirectory = @"C:\Users\HP\Desktop\SimpleSteganography";
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = openDialog.FileName.ToString();
+                pictureBox1.ImageLocation = textBoxFilePath.Text;
+            }
+            textBoxMessage.Text = "";
+        }
+
+        private void buttonEncode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(textBoxFilePath.Text);
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < textBoxMessage.TextLength)
+                    {
+                        Console.WriteLine("R = [" + i + "][" + j + "] = " + pixel.R);
+                        Console.WriteLine("G = [" + i + "][" + j + "] = " + pixel.G);
+                        Console.WriteLine("B = [" + i + "][" + j + "] = " + pixel.B);
+
+                        char letter = Convert.ToChar(textBoxMessage.Text.Substring(j, 1));      //substring = jden başlar 1 karakter alır
+                        int value = Convert.ToInt32(letter);                                    //message harfi tek tek alınarak ascii koduna çevrilir
+                        Console.WriteLine("letter : " + letter + " value : " + value);
+
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
+                    }
+
+                    if (i == img.Width - 1 && j == img.Height - 1)
+                    {
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, textBoxMessage.TextLength));
+                    }
+                }
+            }
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "İmage Files (*.png, *.jpg) | *.png; *.jpg";
+            saveFile.InitialDirectory = @"C:\Users\HP\Desktop\SimpleSteganography";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = saveFile.FileName.ToString();
+                pictureBox1.ImageLocation = textBoxFilePath.Text;
+                PSNR_MSE(/*textBoxFilePath.Text*/);
+
+                img.Save(textBoxFilePath.Text);
+            }
+
+            int boyut = img.Width * img.Height;  //resmin boyutunu öğrenme
+            int bit = boyut * 3;                //saklanacak veri için olan yer
+            mesajLabel.Text = bit + " bit gizleyebilirsiniz.";
+
+
+            //PSNR_MSE(textBoxFilePath.Text);
+        }
+
+        private void buttonDecode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(textBoxFilePath.Text);
+            string message = "";
+
+            Color lastpixel = img.GetPixel(img.Width - 1, img.Height - 1);
+            int msgLength = lastpixel.B;
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j < msgLength)
+                    {
+                        int value = pixel.B;
+                        char c = Convert.ToChar(value);
+                        string letter = System.Text.Encoding.ASCII.GetString(new byte[] { Convert.ToByte(c) });
+
+                        message = message + letter;
+                    }
+                }
+            }
+
+            textBoxMessage.Text = message;
+
+            int length = message.Length; //metin uzunluğu
+            int bit = length * 8;           //metnin bit değeri
+            mesajLabel.Text = bit + " bit gizlenmiştir.";
+
+        }
+
+
+
+        private void PSNR_MSE(/*string ts*/)
+        {
+            Bitmap NImage = new Bitmap("C:/Users/HP/Desktop/SimpleSteganography/lena.png");
+            Bitmap OImage = new Bitmap("C:/Users/HP/Desktop/SimpleSteganography/lena.png");
+            if (OImage == NImage) { Console.WriteLine("Image"); }
+
+
+            double PSNR = 0;
+            double MSE = 0;
+            double R = 0;
+            double G = 0;
+            double B = 0;
+
+            for (int i = 0; i < OImage.Width; i++)
+            {
+                for (int j = 0; j < OImage.Height; j++)
+                {
+                    Color O = OImage.GetPixel(i, j);
+                    Color N = NImage.GetPixel(i, j);
+
+                    Color O1 = OImage.GetPixel(i, j);
+                    int Ot = O1.R & Convert.ToInt32("00000000000000000000000000000001", 2);
+
+                    Color N1 = NImage.GetPixel(i, j);
+                    int Nt = N1.R & Convert.ToInt32("00000000000000000000000000000001", 2);
+
+                    int RO, RN;
+                    RO = OImage.GetPixel(i, j).R;
+                    RN = NImage.GetPixel(i, j).R;
+
+                    int GO, GN;
+                    GO = OImage.GetPixel(i, j).G;
+                    GN = NImage.GetPixel(i, j).G;
+
+                    int BO, BN;
+                    BO = OImage.GetPixel(i, j).B;
+                    BN = NImage.GetPixel(i, j).B;
+
+
+                    int RS = 0;
+                    int GS = 0;
+                    int BS = 0;
+
+                    RS = RO - RN;
+                    RS = RS * RS;
+                    R += RS;
+
+                    GS = GO - GN;
+                    GS = GS * GS;
+                    G += GS;
+
+                    BS = BO - BN;
+                    BS = BS * BS;
+                    B += BS;
+
+                }
+
+            }
+
+            double hw = (OImage.Width * OImage.Height);
+            double sum = R + G + B;
+
+            MSE = sum / hw;
+
+            double S255 = 255 * 255;
+            double S255_Mse = S255 / MSE;
+            double Lo = Math.Log10(S255_Mse);
+
+            PSNR = 10 * Lo;
+
+            MSELabel.Text = MSE.ToString();
+            PSNRLabel.Text = PSNR.ToString();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            mesajLabel.Text = "0 karakter gizleyebilirsiniz.";
+        }
+
+        private void buttonRGB_Click(object sender, EventArgs e)
+        {
+            Image img = pictureBox1.Image;
+            Bitmap bmp = new Bitmap(img); //Bitmap sınıfından bmp nesnesi tanımlandı
+            int[] kirmizi = new int[256];
+            int[] yesil = new int[256];
+            int[] mavi = new int[256];
+
+            for (int i = 0; i < bmp.Size.Height; i++)
+            {
+                for (int j = 0; j < bmp.Size.Width; j++)
+                {
+                    Color renk = bmp.GetPixel(i, j);
+                    kirmizi[renk.R]++;
+                    yesil[renk.G]++;
+                    mavi[renk.B]++;
+                }
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = ".";
+            sfd.Title = "değerlerin kaydedileceği dosyayı belirtin";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter fwriter = File.CreateText(sfd.FileName);
+                for (int i = 0; i < 256; i++)
+                {
+                    fwriter.Write("R:" + kirmizi[i] + "| G:" + yesil[i] + "| B:" + mavi[i] + "\n");
+                }
+                fwriter.Close();
+            }
+        }
+    }
+}
